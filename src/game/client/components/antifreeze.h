026e3e54d06de8a6d9dcc0c4a0fe36b5c025c028
zapomnimena35-png <zapomnimena35@gmail.com>
2026-07-14 15:01:15 +0300

    Update antifreeze.h

--- src/game/client/components/antifreeze.h
+++ src/game/client/components/antifreeze.h
@@ -1,1 +1,29 @@
+#pragma once
+#include <game/client/component.h>
 
+class CAntiFreeze : public CComponent
+{
+public:
+    CAntiFreeze();
+    void OnRender() override;
+    void OnConsoleInit() override;
+    void OnReset() override;
+    void OnRelease() override;
+
+    // НАСТРОЙКИ
+    bool m_Enabled = true;
+    int m_MinTicks = 1;
+    int m_MaxTicks = 4;
+    int m_Hotkey = KEY_X;
+    bool m_IgnoreHookedPlayer = true;
+    bool m_IgnoreOnGround = false;
+    bool m_IgnoreVertical = true;
+
+    // СОСТОЯНИЯ
+    bool m_WasReleased = false;
+    bool m_ToggleKeyPressed = false;   // для бинда на V
+
+private:
+    void ReleaseHook();
+    int GetAdaptiveTicks(float speed);
+};
